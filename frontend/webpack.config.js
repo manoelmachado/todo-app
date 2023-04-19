11e64cc253d41664400233d7dbc652507052a790
manoelmachado <m.machado49@hotmail.com>
2023-04-19 11:08:59 -0300

    alterando arquivos do webpack e package.json

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,25 +4,24 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 module.exports = {
     entry: './src/index.jsx',
     output: {
-        path: __dirname+ '/public',
+        path: __dirname + '/public',
         filename: './app.js'
     },
     devServer: {
-        inline: false,
         port: 8080,
-        contentBase: './public'
+        contentBase: './public',
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules'
         }
     },
-    plugins: [
+    plugins: [ 
         new ExtractTextPlugin('app.css')
     ],
     module: {
-        rules: [{
+        loaders: [{
             test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
@@ -32,9 +31,9 @@ module.exports = {
             }
         }, {
             test: /\.css$/,
-            loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
-            test: /\.woff|.woff2|.tff|.eot|.svg*.*$/,
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
             loader: 'file'
         }]
     }
