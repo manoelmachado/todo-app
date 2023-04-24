5aeedc8aad9ffba5a4b70f13570479378ce98ff6
manoelmachado <m.machado49@hotmail.com>
2023-04-24 17:34:09 -0300

    Update app.jsx

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -5,9 +5,9 @@ import React from 'react'
 import Menu from '../template/menu'
 import Routes from './routes'
 
-export default props => (
-    <div className="container">
+export default props => {
+    <div className='container'>
         <Menu />
         <Routes />
-    </div>>
-)
+    </div>
+}
